4fe4ce89206b4d2e6ce2c5a9c69f5e225242f1c3
Cristian936 <56319886+Cristian936@users.noreply.github.com>
2019-10-29 14:00:28 -0600

    Create hora.c

---
+++ hora.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <time.h>
+#include <windows.h>
+
+int main()
+{
+while (1){
+time_t tiempo = time(NULL);
+struct tm *hora = localtime(&tiempo);
+char output[10];
+strftime(output,10,"%H:%M:%S",hora);
+printf("La hora actual es: %s \n",output);
+Sleep(60000);
+
+}
+return 0;
+}
+
