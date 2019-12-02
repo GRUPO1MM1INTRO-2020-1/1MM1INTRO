4463bf6350bd55a58046c206a52db2763fdceca3
Noe934 <56364026+Noe934@users.noreply.github.com>
2019-12-02 17:34:04 -0600

    Create Apuntadores _a_funcion.c

---
+++ APUNTADORES/Apuntadores _a_funcion.c
@@ -0,0 +1,29 @@
+/** ApuntadoresAFuncion.c
+ * Apuntadores a funci\'on
+ */ 
+//directivas
+
+int (*f)(int,int);
+int (*g)(int,int);
+void Funcion(int (*)(int,int),int,int);
+int mcd(int,int);
+int d(int,int);
+int main(){
+  int A,B;
+// ... inicializar A y B
+  f=mcd; 
+  g=&d;
+  Funcion(f,A,B);
+  Funcion(g,A,B);
+  return 0;
+}/*end main()*/
+
+int mcd(int N,int D)
+{
+  return D%N;
+}
+
+int d(int X,int Y)
+{
+  return Y-X;
+}
