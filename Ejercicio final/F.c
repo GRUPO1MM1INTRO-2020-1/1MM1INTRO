87b6cfa2e8687c757f6416d0ee45fb57aaf9ca6d
Noe934 <56364026+Noe934@users.noreply.github.com>
2019-12-02 17:26:54 -0600

    Create F.c

---
+++ Ejercicio final/F.c
@@ -0,0 +1,31 @@
+int f(int X, int Y){
+	int res;
+	
+	if ( X==2 ){
+			if(Y==0)
+			res=28;
+			else if(Y==1)
+			res=29;
+			else
+			res=30;
+		}
+	else if((X>=0)&&(X<=6)){
+		if ( X%2==0 )
+		res=30;
+    	else
+		res=31;
+	}
+	else if((X>=7)&&(X<=8)){
+		res=31;
+	}
+	else if((X>=9)&&(X<=12)){
+		if ( X%2==0 )
+		res=31;
+    	else
+		res=30;
+	}
+	else{
+	res=2020640450;
+	}
+	return res;
+}
